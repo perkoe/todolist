88dc7b4e8a48d6e0dba4159526639ce6de9a4b6b
perkoe <perkovic.ema@gmail.com>
2024-08-05 21:02:53 +0200

    project setup

---
+++ todolist/pages/index.tsx
@@ -0,0 +1,9 @@
+import { ChakraProvider } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+      <ChakraProvider>
+        <div>To do list</div>
+      </ChakraProvider>
+  );
+}
